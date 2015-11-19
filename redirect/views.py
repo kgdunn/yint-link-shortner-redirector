455451cfdb3e585e21b4a087d6f50c37a8ee0b8a
Kevin Dunn <kgdunn@gmail.com>
2015-11-19 18:15:23 +0100

    Now serves static files too.

--- redirect/views.py
+++ redirect/views.py
@@ -5,21 +5,47 @@ from . import models
 logger = logging.getLogger(__name__)
 logger.debug('A new call to the views.py file')
 
+
+from django.conf import settings
+
+class ForceStatic(Exception):
+    pass
+
+def track_statistics(request, redirect):
+    """Track statistics related to each link request"""
+    pass
+
 def show_blank_home(request):
     return HttpResponse('tiny; a personalized <a href="https://en.'
                         'wikipedia.org/wiki/URL_shortening">link shortener</a> '
                         'service')
 
 def do_redirect(request, srcuri):
-    #if srcuri.lower() == 'favicon.ico':
-    #    return Http404('')
+    """ Handle the redirect.
 
+    Does the ``srcuri`` exist as an object in our database?
+       YES:
+           Does it begin with "http"?
+               YES: then track it (stats), and do the redirect.
+               NO:  append the STATIC_URL in front, and return it back
+
+       NO: it might be static media on the server. Serve it.
+           Obviously if it is not static media, then the server will 404.
+    """
     srcuri = srcuri.replace('$','').replace('(', '').replace(')', '')
     try:
-        source = models.Redirect.objects.get(source=srcuri.strip())
+        redirect = models.Redirect.objects.get(source=srcuri.strip())
         logger.debug('{0}'.format(srcuri))
-        return HttpResponseRedirect(source.destination, status=301)
+        track_statistics(request, redirect)
+        if redirect.destination.startswith('http'):
+            return HttpResponseRedirect(redirect.destination, status=301)
+        else:
+            raise ForceStatic()
+
+    except ForceStatic:
+        logger.warn('FORCED MEDIA: {0}'.format(srcuri))
+        return HttpResponseRedirect(settings.STATIC_URL + redirect.destination)
 
     except models.Redirect.DoesNotExist:
-        logger.warn('NOT FOUND: {0}'.format(srcuri))
-        return show_blank_home(request)
+        logger.warn('FAIL: {0}'.format(srcuri))
+        raise Http404('Not found.')
--- yint/settings.py
+++ yint/settings.py
@@ -101,6 +101,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/Users/kevindunn/Dropbox/Coursera/yint/static-media/'
+STATICFILES_DIRS = (
+    "/Users/kevindunn/Dropbox/Coursera/yint/media-to-copy/",
+)
+
 
 TEMPLATE_DEBUG = True
 
