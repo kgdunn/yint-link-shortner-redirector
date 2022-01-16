4769edc608c08def46b539e9127bd2625fcf8506
Kevin Dunn <kgdunn@gmail.com>
2022-01-16 19:36:55 +0100

    Adding static files setting.

--- yint/settings.py
+++ yint/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / STATIC_URL.strip("/")
 
 
 # Default primary key field type
